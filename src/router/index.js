5850e3e4da858706e4df7f4fb0aac08938910163
Shcherbyna Dmytro <shcherbyna.dmytro.w@gmail.com>
2022-07-04 14:08:15 +0300

    feat: Init commit

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+function loadPage(view) {
+  return () =>
+    import(
+      /* webpackChunkName: "view-[request]" */ `@/views/${view}.vue`
+    );
+}
+
+const routes = [
+      {
+        path: '/',
+        name: 'home',
+        component: loadPage('HomeView')
+      },
+//   {
+//     path: '/about',
+//     name: 'about',
+//     // route level code-splitting
+//     // this generates a separate chunk (about.[hash].js) for this route
+//     // which is lazy-loaded when the route is visited.
+//     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+//   }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
