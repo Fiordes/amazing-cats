516286e72f10c421970b882d0f1f203080884d9e
Shcherbyna Dmytro <shcherbyna.dmytro.w@gmail.com>
2022-07-09 18:10:48 +0300

    feat: breed description to the BreedView component

--- src/views/BreedView.vue
+++ src/views/BreedView.vue
@@ -10,12 +10,41 @@
         <Splide :options="slideOptions">
           <SplideSlide v-for="item in breed" :key="item.id">
             <div class="slide-content" :style="{backgroundImage: `url(${item.url})`}"></div>
-            <!--            <img class="slide-image" :src="" :alt="item.breeds[0].name">-->
           </SplideSlide>
         </Splide>
+
+        <div class="view-breed__description">
+          <h2>{{ breedOption.breedName }}</h2>
+          <h3>{{ breedOption.breedDescription }}</h3>
+          <div class="description-grid">
+            <div class="description-grid__item">
+              <h5>Temperament</h5>
+              <p>{{ breedOption.breedTemperament }}</p>
+            </div>
+            <div class="description-grid__item">
+              <ul>
+                <li>
+                  <bold>Origin:</bold>
+                  <span>{{ breedOption.breedOrigin }}</span>
+                </li>
+                <li>
+                  <bold>Weight:</bold>
+                  <span>{{ breedOption.breedWeight }} kgs</span>
+                </li>
+                <li>
+                  <bold>Life span:</bold>
+                  <span>{{ breedOption.breedLife }} years</span>
+                </li>
+              </ul>
+            </div>
+
+          </div>
+        </div>
       </div>
     </template>
-
+    <template v-else>
+      <div>LOADING</div>
+    </template>
 
   </div>
 
@@ -39,6 +68,16 @@ export default {
     const breed = computed(() => {
       return store.state.breed
     });
+    const breedOption = computed(() => {
+      return {
+        breedName: breed.value[0].breeds[0].name,
+        breedDescription: breed.value[0].breeds[0].description,
+        breedTemperament: breed.value[0].breeds[0].temperament,
+        breedOrigin: breed.value[0].breeds[0].origin,
+        breedWeight: breed.value[0].breeds[0].weight.metric,
+        breedLife: breed.value[0].breeds[0].life_span
+      }
+    })
 
     const slideOptions = {
       type: 'loop',
@@ -60,7 +99,8 @@ export default {
     return {
       breed,
       id,
-      slideOptions
+      slideOptions,
+      breedOption
     }
   }
 
@@ -68,8 +108,6 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
-
 .view {
   &-title {
     background: $color-peach-secondary;
@@ -88,14 +126,68 @@ export default {
       margin-left: 10px;
       text-transform: uppercase;
     }
-  }
-}
 
-//.slide-image {
-//  max-height: 360px;
-//  height: 100%;
-//  object-fit: fill;
-//}
+    &__description {
+      margin-top: 51px;
+      padding: 0 40px 40px;
+      border: 2px solid $color-peach-secondary;
+      border-radius: 20px;
+      position: relative;
+
+      h2 {
+        font: 500 36px/52px $main-font, sans-serif;
+        text-align: center;
+        background: #FFFFFF;
+        border-radius: 20px;
+        padding: 5px 40px;
+        max-width: fit-content;
+        position: relative;
+        top: -32px;
+        left: 50%;
+        transform: translateX(-50%);
+      }
+
+      h3 {
+        font: $main-font-size;
+        margin-bottom: 20px;
+        text-align: center;
+        color: $color-gray;
+      }
+
+      .description-grid {
+        display: flex;
+
+        &__item {
+          h5 {
+            font: 500 16px/23px $main-font, sans-serif;
+            color: $color-main;
+          }
+
+          p {
+            max-width: 270px;
+            font: 400 16px/23px $main-font, sans-serif;
+            color: $color-gray;
+          }
+
+          li {
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+            font: 500 16px/23px $main-font, sans-serif;
+
+            span {
+              color: $color-gray;
+              margin-left: 5px;
+              font: 400 16px/23px $main-font, sans-serif;
+            }
+          }
+        }
+      }
+    }
+  }
+
+
+}
 
 .slide-content {
   height: 360px;
